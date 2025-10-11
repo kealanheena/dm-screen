2ad72d05881da82217573569f30843d9e4f3df12
Kealan Heena <kealanheena@gmail.com>
2025-10-11 02:09:59 -0700

    Feat: update some colors in the globals.css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import { currentUser } from "@clerk/nextjs/server";
 
 function Navbar() {
   return (
-    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+    <nav className="sticky top-0 w-full border-b border-b-(--primary) bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
     	<div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
