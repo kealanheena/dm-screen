a64176689780a762220d08ee4eb97982f1da1148
Kealan Heena <kealanheena@gmail.com>
2025-10-12 21:45:30 -0700

    Feat: add an action that creates a user using Clerk data

---
+++ src/actions/user.action.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+
+export async function syncUser() {
+
+	try {
+		const { userId } = await auth();
+		const user = await currentUser();
+
+		if (!userId && !user) {
+			return;
+		}
+
+		const existingUser = await prisma.user.findUnique({
+			where: {
+				clerkId: userId,
+			}
+		});
+
+		if (existingUser) {
+			return existingUser;
+		}
+
+		const email = user.emailAddresses[0].emailAddress;
+
+		const dbUser = await prisma.user.create({
+			data: {
+				clerkId: userId,
+				name: `${user.firstName || ""} ${user.lastName || ""}`,
+				username: user.username ?? email.split('@')[0],
+				email,
+				image: user.imageUrl,
+			}
+		})
+
+		return dbUser;
+	} catch (error) {
+		console.log('Error in syncUser', error)
+	}
+	
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,4 @@
-import { Button } from '@/components/ui/button';
-import ModeToggle from '@/components/ModeToggle';
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className='m-4'>
       dashboard
