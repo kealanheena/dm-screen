7f3b2e6d1d02066e757c6fe70c559a595841baa8
Kealan Heena <kealanheena@gmail.com>
2025-10-13 22:54:35 -0700

    Feat: Revalidate path to update next cache

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 
 export async function syncUser() {
@@ -162,6 +163,8 @@ export async function toggleFollow(targetUserId: string) {
 				})
 			]);
 		}
+		// Reset the cache to update the page
+		revalidatePath("/");
 
 		return { success: true };
 	} catch (error) {
