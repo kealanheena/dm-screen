136480aceacb059d1145ac4903071a03021b1b4a
Kealan Heena <kealanheena@gmail.com>
2025-10-14 20:48:57 -0700

    Feat: add state to PostCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,21 @@ import { getPosts } from "@/actions/post.action";
 import CreatePost from "@/components/CreatePost";
 import WhoToFollow from "@/components/WhoToFollow";
 import PostCard from "@/components/PostCard";
+import { getDbUserId } from "@/actions/user.action";
 
 export default async function Home() {
   const user = await currentUser();
   const posts = await getPosts();
+  const dbUserId = await getDbUserId();
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-10 gap-6'>
       <div className='lg:col-span-6'>
         {user ? <CreatePost /> : null}
+
         <div className="space-y-6">
           {posts.map(post => (
-            <PostCard key={posts.id} post={post} />
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
           ))}
         </div>
       </div>
