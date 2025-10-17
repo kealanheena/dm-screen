0a91543bd0be59c9bd782c389323850360bdbeb8
Kealan Heena <kealanheena@gmail.com>
2025-10-17 15:06:00 -0700

    Feat: add second screen and render it correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import { ForwardRefExoticComponent, RefAttributes } from "react";
 
 interface Tab {
   title: string;
-  column: number;
+  section: number;
   height: string;
-  type: 'TEXT';
+  type: 'TEXT' | 'MAP';
 }
 
 type SectionNumber = 1 | 2 | 3 | 4 | 6 | 12;
@@ -14,66 +14,63 @@ type SectionNumber = 1 | 2 | 3 | 4 | 6 | 12;
 interface Screen {
   title: string;
   key: string;
-  sectionNumber: SectionNumber;
+  sectionsAmount: SectionNumber;
   tabs: Tab[];
 }
 
-const screens: Screen[] = [{
-  title: 'Intro',
-  key: 'intro',
-  sectionNumber: 1,
-  tabs: [{
-    title: 'Opening',
-    column: 0,
-    height: '100%',
-    type: 'TEXT'
-  }]
-}];
+const screens: Screen[] = [
+  {
+    title: 'Intro',
+    key: 'intro',
+    sectionsAmount: 1,
+    tabs: [{
+      title: 'Opening',
+      section: 0,
+      height: '100%',
+      type: 'TEXT'
+    }], 
+  }, {
+    title: 'Map',
+    key: 'map',
+    sectionsAmount: 2,
+    tabs: [{
+      title: 'Opening',
+      section: 0,
+      height: '50%',
+      type: 'TEXT'
+    }], 
+  }
+];
 
 export default async function Home() {
+  const currentTab = screens[0].key;
 
   return (
-    <Tabs defaultValue="posts" className="w-full">
+    <Tabs defaultValue={currentTab} className="w-full">
         <TabsList className="flex w-full justify-start border-b rounded-none h-auto p-0 bg-transparent">
-          {screens.map(({ title, key, icon }: Screen) => (
+          {screens.map(({ title, key }: Screen) => (
             <TabsTrigger
-              value="posts"
+              key={key}
+              value={key}
               className="flex items-center gap-4 rounded-t-lg border-2 border-background data-[state=active]:border-primary data-[state=active]:bg-primary px-6 font-semibold"
             >
               {title}
             </TabsTrigger>
           ))}
-          
-          <TabsTrigger
-            value="likes"
-            className="flex items-center gap-4 rounded-t-lg border-2 border-background data-[state=active]:border-primary data-[state=active]:bg-primary px-6 font-semibold"
-          >
-            <MapIcon className="size-4" />
-            Map
-          </TabsTrigger>
         </TabsList>
 
-        <TabsContent value="posts" className="mt-6">
-          <div className="space-y-6">
-            {/* {posts.length > 0 ? (
-              posts.map((post) => <PostCard key={post.id} post={post} dbUserId={user.id} />)
-            ) : (
-              <div className="text-center py-8 text-muted-foreground">No posts yet</div>
-            )} */}
-            Posts
-          </div>
-        </TabsContent>
-
-        <TabsContent value="likes" className="mt-6">
-          <div className="space-y-6">
-            Likes
-            {/* {likedPosts.length > 0 ? (
-              likedPosts.map((post) => <PostCard key={post.id} post={post} dbUserId={user.id} />)
-            ) : (
-              <div className="text-center py-8 text-muted-foreground">No liked posts to show</div>
-            )} */}
-          </div>
-        </TabsContent>
+        {screens.map(({ key }) => (
+          <TabsContent value={key} className="mt-6">
+            <div className="space-y-6">
+              {/* {posts.length > 0 ? (
+                posts.map((post) => <PostCard key={post.id} post={post} dbUserId={user.id} />)
+              ) : (
+                <div className="text-center py-8 text-muted-foreground">No posts yet</div>
+              )} */}
+              {key}
+            </div>
+          </TabsContent>
+        ))}
       </Tabs>
   );
 }
