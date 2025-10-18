4124b7ae8632b541aea41b745f738b9a31ae7924
Kealan Heena <kealanheena@gmail.com>
2025-10-18 15:02:54 -0700

    Feat: add a basic Navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from "@/components/ThemeProvider"
 
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar"
-import ImageBackground from "@/components/ImageBackground";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "DM Screen",
-  description: "Create DM Screens for all your Table Top RPG needs",
-};
 
 export default function RootLayout({
   children,
@@ -31,10 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      
       <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        <body style={{ margin: 0 }}
         >
           {/* <ThemeProvider
             attribute="class"
@@ -42,8 +23,9 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           > */}
-            <div className="flex flex-col h-screen">
-              <main className="pb-8 px-4 grow">
+            <div>
+              <Navbar />
+              <main>
                 {children}
               </main>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,9 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
-import { PlusIcon, MapIcon, LucideProps } from "lucide-react";
-import { ForwardRefExoticComponent, RefAttributes } from "react";
-
-interface Section {
-  title: string;
-  key: string;
-  section: Section[];
-  sidebar: Section[];
-  height: string;
-  type: 'TEXT' | 'MAP';
-}
-
-interface Screen {
-  title: string;
-  key: string;
-}
-
-const screens: Screen[] = [
-  {
-    title: 'Intro',
-    key: 'intro',
-    section: [{
-      title: 'Opening',
-      key: 'opening',
-      section: 0,
-      height: '100%',
-      type: 'TEXT'
-    }, {
-      title: 'Map Image',
-      key: 'map_image',
-      section: 0,
-      height: '50%',
-      type: 'TEXT'
-    }], 
-  },
-];
+import React from "react";
 
 export default async function Home() {
-  const screen = screens[0];
-
-  const { tabs, sectionsAmount } = screen;
 
   return (
-  <div className={`flex flex-row h-full w-full`}>
+  <div>
     test
   </div>
   );
