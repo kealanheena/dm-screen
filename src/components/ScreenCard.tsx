1077a0bfc4f8adb1cb050f8dbfecb47a9d18918a
Kealan Heena <kealanheena@gmail.com>
2025-10-18 11:48:43 -0700

    Feat: add flex ratios to the columns

--- src/components/ScreenCard.tsx
+++ src/components/ScreenCard.tsx
@@ -5,15 +5,45 @@ import React, { useState } from 'react';
 import { Card, CardContent, CardTitle } from "./ui/card";
 import { map, filter } from 'lodash';
 import { Button } from './ui/button';
-import { PlusIcon, Trash2Icon, Loader2Icon } from 'lucide-react';
+import { ArrowLeftFromLineIcon, ArrowRightFromLineIcon, Trash2Icon, Loader2Icon } from 'lucide-react';
 import toast from 'react-hot-toast';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
 
 function ScreenCard({ onClickDelete, isDeleting }) {
+	const [ratio, setRatio] = useState(1);
+
+	const increaseRatio = () => {
+		setRatio(ratio + 1)
+	}
+
+	const decreaseRatio = () => {
+		setRatio(ratio - 1)
+	}
 
 
 	return (
-		<Card className='flex-1 h-full mx-1'>
-			{/* <div > */}
+		<Card className={`flex-${ratio} h-full mx-1`}>
+				<div className="flex justify-between m-0 p-0">
+					<Button
+						variant="ghost"
+						size="sm"
+						className="text-muted-foreground hover:text-green-500"
+						onClick={decreaseRatio}
+					>
+						<ArrowLeftFromLineIcon className='size-4' />
+					</Button>
+					<Label>Ratio: {ratio}</Label>
+					<Button
+						variant="ghost"
+						size="sm"
+						className="text-muted-foreground hover:text-green-500"
+						onClick={increaseRatio}
+					>
+						<ArrowRightFromLineIcon className='size-4' />
+					</Button>
+				</div>
+
 				<CardTitle className='flex flex-row justify-between items-center mx-6'>
 					Test
 					<Button
@@ -25,7 +55,6 @@ function ScreenCard({ onClickDelete, isDeleting }) {
 						{isDeleting ? <Loader2Icon className="size-4 animate-spin" /> : <Trash2Icon className="size-4" />}
 					</Button>
 				</CardTitle>
-			{/* </div> */}
 
 			<CardContent>Test test test</CardContent>
 		</Card>
