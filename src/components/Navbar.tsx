638291c82abcc498f41f95ed4d984cd6895c2ca7
Kealan Heena <kealanheena@gmail.com>
2025-11-02 12:38:19 -0800

    Feat: remove unused items from AppBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,25 @@
-"use client"
-
 import React from 'react';
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
 import { AppBar, Box, Toolbar, Typography, Button, IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
 
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await currentUser()
+
+  if (user) {
+    syncUser()
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             DM screen
           </Typography>
+
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
