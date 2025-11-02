f11798adb20bee798f3b6ba0bd425b4ac12ffa5b
Kealan Heena <kealanheena@gmail.com>
2025-11-01 23:36:28 -0700

    Feat: add a add button component

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -1,32 +1,30 @@
-"use client"
-
 import React from 'react';
 
 import { Button, IconButton, Tooltip, Typography } from '@mui/material';
-import { Delete } from '@mui/icons-material';
+import { AddCircle } from '@mui/icons-material';
 
-interface DeleteButtonProps { 
+interface AddButtonProps { 
 	onClick: Function;
 	text?: string;
 	tooltip: string;
 	icon?: 'icon_plus_text' | 'icon_only' | 'no_icon';
 }
 
-const DeleteButton = ({
+const AddButton = ({
 	onClick,
 	text,
 	tooltip,
 	icon = 'icon_plus_text',
-}: DeleteButtonProps) => {
+}: AddButtonProps) => {
 	
 	if (icon === 'icon_only') {
 		return (
 			<Tooltip title={tooltip}>
 				<IconButton 
-					color='error'
+					color='success'
 					onClick={onClick}
 				>
-					<Delete />
+					<AddCircle />
 				</IconButton>
 			</Tooltip>
 		);
@@ -36,15 +34,15 @@ const DeleteButton = ({
 		<Button 
 			onClick={onClick}
 			variant="outlined"
-			color="error"
+			color="success"
 		>
 			<Typography
 				sx={{ p: icon !== 'no_icon' ? 0 : 0.5 }}
 			>
 				{text}
 			</Typography>
-			{icon !== 'no_icon' && <Delete />}
+			{icon !== 'no_icon' && <AddCircle />}
 		</Button>
 );
 }
-export default DeleteButton;
+export default AddButton;
