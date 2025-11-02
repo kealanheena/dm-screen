93c3d452bb8d3bc90cfdd240ca9d16bd89701b2d
Kealan Heena <kealanheena@gmail.com>
2025-11-02 01:28:51 -0800

    Feat: update types and constants

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,15 @@
 export interface LayoutType {
 	id: number;
-	start: number; 
-	width: number;
+	// Using MUIs grid which is made up of 12 sections
+	start: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 ;
+	// the 2 makes it a minWidth of 2 sections
+	width: 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 ;
+	cards: CardType[];
 }
 
 export interface CardType {
 	id: number;
 	title: string;
 	height?: number;
-	// Using MUIs grid which is made up of 12 sections
-	// the 2 makes it a minWidth of 2 sections
-	width: 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 ;
 	img?: string;
 }
