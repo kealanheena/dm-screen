fc66063230262bf92b2ee27a2acdd689bc5dcadc
Kealan Heena <kealanheena@gmail.com>
2025-11-01 23:31:47 -0700

    Feat: add a delete button component

---
+++ src/components/DeleteButton.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import React, { useState } from 'react';
+
+import { Button, Card, CardContent, Grid, Paper, Slider, IconButton, Tooltip, Typography } from '@mui/material';
+import { Delete, OpenWith } from '@mui/icons-material';
+import { LayoutType } from '@/types';
+
+interface DeleteButtonProps { 
+	onClick: Function;
+	text?: string;
+	tooltip: string;
+	icon?: 'icon_plus_text' | 'icon_only' | 'no_icon';
+}
+
+const DeleteButton = ({
+	onClick,
+	text,
+	tooltip,
+	icon = 'icon_plus_text',
+}: DeleteButtonProps) => {
+	
+	if (icon === 'icon_only') {
+		return (
+			<Tooltip title={tooltip}>
+				<IconButton 
+					color='error'
+					onClick={onClick}
+				>
+					<Delete />
+				</IconButton>
+			</Tooltip>
+		);
+	}
+
+	return (
+		<Button 
+			onClick={onClick}
+			variant="outlined"
+			color="error"
+		>
+			<Typography
+				sx={{ p: icon !== 'no_icon' ? 0 : 0.5 }}
+			>
+				{text}
+			</Typography>
+			{icon !== 'no_icon' && <Delete />}
+		</Button>
+);
+}
+export default DeleteButton;
