c7655c1aa57e9113fa5ae42aeef7a374f3e84da2
Kealan Heena <kealanheena@gmail.com>
2025-11-05 01:32:02 -0800

    Feat: update Redirect component to return a react fragment

--- src/components/Redirect.tsx
+++ src/components/Redirect.tsx
@@ -1,23 +1,27 @@
 "use client"
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import { includes } from 'lodash';
 import { redirect, RedirectType, usePathname } from 'next/navigation';
 
 import { ACCESSABLEPATHSWITHOUTLOGIN } from '@/constants';
 import { User } from '@clerk/nextjs/server';
 
+type RedirectProps = {
+	user: User | null;
+}
 
-
-function Redirect({ user }: { user: User }) {
+function Redirect({ user }: RedirectProps) {
 	const pathname = usePathname();
 	const isPageAccessable = (
 		user || includes(ACCESSABLEPATHSWITHOUTLOGIN, pathname)
-	)
+	);
 
 	if (!isPageAccessable) {
 		redirect('/log-in', RedirectType.replace)
 	}
+
+	return <Fragment />;
 }
 
 export default Redirect;
