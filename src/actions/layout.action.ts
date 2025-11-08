3e2e1a0e7a7014ee338fc17b91df370473502ccf
Kealan Heena <kealanheena@gmail.com>
2025-11-07 22:07:47 -0800

    Feat: add title to the schema

--- src/actions/layout.action.ts
+++ src/actions/layout.action.ts
@@ -2,11 +2,39 @@
 
 import prisma from "@/lib/prisma";
 
+export async function getDefaultLayout() {
+	try {
+		return prisma.layout.findFirst({
+			where: { isDefault: true },
+			select: {
+				id: true,
+				title: true,
+				blocks: {
+					select: {
+						id: true,
+						start: true,
+						width: true,
+						cards: {
+							select: {
+								title: true,
+								height: true,
+							}
+						},
+					}
+				},
+			}
+		});
+	} catch (error) {
+		throw new Error('Default layout not found (404)');
+	}
+}
+
 export async function getDefaultLayouts() {
  return prisma.layout.findMany({
 		where: { isDefault: true },
 		select: {
 			id: true,
+			title: true,
 			blocks: {
 				select: {
 					id: true,
