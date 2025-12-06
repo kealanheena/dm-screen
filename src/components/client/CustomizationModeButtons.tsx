9d3f5aa6720c3c07be444cd2ce323b9e0aa4ad97
Kealan Heena <kealanheena@gmail.com>
2025-12-05 22:42:56 -0800

    Feat: remove section buttons from action bar

--- src/components/client/CustomizationModeButtons.tsx
+++ src/components/client/CustomizationModeButtons.tsx
@@ -3,32 +3,21 @@
 import React, { Fragment, useContext } from 'react';
 
 import { Divider, Grid, IconButton } from '@mui/material'; 
-import { AddBox, Check, DisabledByDefault, Settings } from '@mui/icons-material';
+import { Check, DisabledByDefault, Settings } from '@mui/icons-material';
 import { ScreenContext } from '@/app/context';
-
-import SectionButton from '@/components/client/SectionButton';
 
 
 
 const CustomizationModeButtons = () => {
-	const { isCustomizing, setIsCustomizing, selectedSection } = useContext(ScreenContext);
+	const { isCustomizing, setIsCustomizing } = useContext(ScreenContext);
 
 	return (
 		<Grid container>
 			{isCustomizing && (
 				<Fragment>
-					<SectionButton sectionAction="ADD" />
-	
-					<IconButton disabled onClick={() => {}}>
-						<AddBox />
+					<IconButton disabled color="error" onClick={() => {}}>
+						<DisabledByDefault />
 					</IconButton>
-
-					<Divider orientation="vertical" variant="middle" flexItem />
-
-					<SectionButton
-						disabled={!selectedSection}
-						sectionAction="DELETE"
-					/>
 
 					<IconButton disabled color="error" onClick={() => {}}>
 						<DisabledByDefault />
