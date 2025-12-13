ac5a218315263d3bc75a618d3ca6e538eb02c108
Kealan Heena <kealanheena@gmail.com>
2025-12-12 21:05:27 -0800

    Feat: update list component to contain the card components

--- src/components/client/Cards/ListComponent.tsx
+++ src/components/client/Cards/ListComponent.tsx
@@ -1,11 +1,12 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { capitalize, filter, includes, map } from "lodash";
 
-import { OpenInNew, Person } from "@mui/icons-material";
-import { CircularProgress, Grid, IconButton, List, ListItem, ListItemText, TextField, Typography } from "@mui/material";
+import { Groups, OpenInNew, Person } from "@mui/icons-material";
+import { Card, CardContent, CircularProgress, Grid, IconButton, List, ListItem, ListItemText, TextField, Typography } from "@mui/material";
 import { getPlayerCharacters } from "@/actions/playerCharacter.action";
+import PlayerCharacterFormDialog from "../PlayerCharacterFormDialog";
 
-const ListComponent = () => {
+const ListComponent = ({ card }: { card: { id: number, title: string }}) => {
 	const [search, setSearch] = useState('');
 	const [items, setItems] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
@@ -24,69 +25,83 @@ const ListComponent = () => {
 		}
 	}, []);
 
+	console.log(items)
+
 	const filteredItems = filter(items, ({ name }) => includes(name, search));
 
 	return (
-		<Grid
-			sx={{
-				bgcolor: 'background.paper',
-				position: 'relative',
-				overflow: 'scroll',
-				height: '100%',
-				maxHeight: '100%',
-			}}
-		>
-			<TextField
-				placeholder="Search ..."
-				value={search}
-				onChange={(e) => setSearch(e.target.value)}
-				fullWidth
-			/>
-			{isLoading ? (
-				<Grid display="flex" flexDirection="column" alignItems="center">
-					<br/>
-					<CircularProgress />
-				</Grid>
-			) : (
-				<List
-					sx={{
-						'& ul': { padding: 0 },
-					}}
-				>
-					{map(filteredItems, (item) => (
-						<ListItem key={`player_character_${item.id}`}>
-							<ListItemText
-								primary={
-									<Grid display="flex">
-										<Person />
-										<Typography sx={{ pl: 1 }} >{item.name}</Typography>
-									</Grid>
-								}
-								secondary={
-									item?.class && (
-										<Fragment>
-											<Typography component="span" variant="body2">{`class: ${item.class.name}`}</Typography>
-											<Typography variant="body2">{`species:
-												${item.subspecies?.name ? ` ${capitalize(item.subspecies?.name)} `  : ''}
-												${capitalize(item.species?.name)}
-											`}</Typography>
-											
-										</Fragment>
-									)
-								}
-								sx={{ pl: 0.5 }} 
-							/>
-							{item?.url && (
-								<IconButton onClick={() => window.open(item.url, '_blank')}>
-									<OpenInNew />
-								</IconButton>
-							)}
-						</ListItem>
-					))}
-					<br /> 
-				</List>
-			)}
-		</Grid>
+		<Card>
+				<CardContent sx={{ height: '100%'}}>
+					<Grid container justifyContent="space-between">
+						<Grid display="flex" alignItems="center">
+							<Groups color='primary'/>
+							<Typography sx={{ pl: 1 }} variant="h6">{card.title}</Typography>
+						</Grid>
+
+						<PlayerCharacterFormDialog />
+					</Grid>
+					<Grid
+						sx={{
+							bgcolor: 'background.paper',
+							position: 'relative',
+							overflow: 'scroll',
+							height: '100%',
+							maxHeight: '100%',
+						}}
+					>
+						<TextField
+							placeholder="Search ..."
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+							fullWidth
+						/>
+						{isLoading ? (
+							<Grid display="flex" flexDirection="column" alignItems="center">
+								<br/>
+								<CircularProgress />
+							</Grid>
+						) : (
+							<List
+								sx={{
+									'& ul': { padding: 0 },
+								}}
+							>
+								{map(filteredItems, (item) => (
+									<ListItem key={`player_character_${item.id}`}>
+										<ListItemText
+											primary={
+												<Grid display="flex">
+													<Person />
+													<Typography sx={{ pl: 1 }} >{item.name}</Typography>
+												</Grid>
+											}
+											secondary={
+												item?.class && (
+													<Fragment>
+														<Typography component="span" variant="body2">{`class: ${item.class.name}`}</Typography>
+														<Typography variant="body2">{`species:
+															${item.subspecies?.name ? ` ${capitalize(item.subspecies?.name)} `  : ''}
+															${capitalize(item.species?.name)}
+														`}</Typography>
+														
+													</Fragment>
+												)
+											}
+											sx={{ pl: 0.5 }} 
+										/>
+										{item?.url && (
+											<IconButton onClick={() => window.open(item.url, '_blank')}>
+												<OpenInNew />
+											</IconButton>
+										)}
+									</ListItem>
+								))}
+								<br /> 
+							</List>
+						)}
+					</Grid>
+			</CardContent>
+		</Card>
 	)
 };
 
