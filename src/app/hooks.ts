947be501123cd864eec89d0e84f231a34f23780e
Kealan Heena <kealanheena@gmail.com>
2025-12-14 21:06:22 -0800

    Feat: add hooks for getting initial data

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,16 +1,61 @@
-import React from 'react';
-import { debounce }  from 'lodash';
+import{
+  useContext,
+  useEffect, 
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import { debounce, map }  from 'lodash';
+import { ScreenContext } from './context';
+import { getPlayerCharacters } from '@/actions/playerCharacter.action';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function useDebounce<CallbackType extends (...args: any[]) => any>(callback: CallbackType, delay: number) {
-  const callbackRef = React.useRef(callback)
+  const callbackRef = useRef(callback)
 
-  React.useLayoutEffect(() => {
+  useLayoutEffect(() => {
     callbackRef.current = callback
   })
 
-  return React.useMemo(
+  return useMemo(
     () => debounce((...args) => callbackRef.current(...args), delay),
     [delay],
   )
-}
+};
+
+// 
+export function useFetchScreenData() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const context = useContext(ScreenContext);
+
+  if (context === undefined) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+
+  useEffect(() => {
+    const fetchListData = async () => {
+      try {
+        const result = await getPlayerCharacters();
+        
+        // console.log({result});
+
+        setData(result);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchListData();
+  }, []); // The effect runs again if the URL changes
+
+  // Return the necessary states for components to use
+  return { data, isLoading, error };
+};
