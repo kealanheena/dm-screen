74763c2fb9cc7c9ad57fdb42dfb2eccc573932db
Kealan Heena <kealanheena@gmail.com>
2026-01-08 22:48:00 -0800

    Feat: update db seed to use the snake_case

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,334 +3,266 @@ import { AbilityScore, PrismaClient } from "@prisma/client";
 const species = [{
   key: 'aasimar',
   name: 'Aasimar',
-  updatedAt: new Date(),
 },
 {
   key: 'changling',
   name: 'Changling',
-  updatedAt: new Date(),
 }, {
   key: 'dragonborn',
   name: 'Dragonborn',
-  updatedAt: new Date(),
   subspecies: [
-    { key: 'dragonborn_black', name: 'Black' },
-    { key: 'dragonborn_blue', name: 'Blue' },
-    { key: 'dragonborn_brass', name: 'Brass' },
-    { key: 'dragonborn_bronze', name: 'Bronze' },
-    { key: 'dragonborn_copper', name: 'Copper' },
-    { key: 'dragonborn_gold', name: 'Gold' }, 
-    { key: 'dragonborn_green', name: 'Green' },
-    { key: 'dragonborn_red', name: 'Red' },
-    { key: 'dragonborn_silver', name: 'Silver' },
-    { key: 'dragonborn_white', name: 'White' },
+    { key: 'black', name: 'Black' },
+    { key: 'blue', name: 'Blue' },
+    { key: 'brass', name: 'Brass' },
+    { key: 'bronze', name: 'Bronze' },
+    { key: 'copper', name: 'Copper' },
+    { key: 'gold', name: 'Gold' }, 
+    { key: 'green', name: 'Green' },
+    { key: 'red', name: 'Red' },
+    { key: 'silver', name: 'Silver' },
+    { key: 'white', name: 'White' },
   ]
 }, {
   key: 'dwarf',
   name: 'Dwarf',
-  updatedAt: new Date(),
 }, {
   key: 'elf',
   name: 'Elf',
-  updatedAt: new Date(),
   subspecies: [
-    { key: 'elf_drow', name: 'Drow' },
-    { key: 'elf_high', name: 'High' },
-    { key: 'elf_wood', name: 'Wood' },
+    { key: 'drow', name: 'Drow' },
+    { key: 'high', name: 'High' },
+    { key: 'wood', name: 'Wood' },
   ]
 }, {
   key: 'gnome',
   name: 'Gnome',
-  updatedAt: new Date(),
   subspecies: [
-    { key: 'gnome_forest', name: 'Forest' },
-    { key: 'gnome_rock', name: 'Rock' },
+    { key: 'forest', name: 'Forest' },
+    { key: 'rock', name: 'Rock' },
   ]
 }, {
   key: 'hexblood',
   name: 'Hexblood',
-  updatedAt: new Date(),
 }, {
   key: 'goliath',
   name: 'Goliath',
-  updatedAt: new Date(),
   subspecies: [
-    { key: 'goliath_cloud_giant', name: 'Cloud Giant' },
-    { key: 'goliath_fire_giant', name: 'Fire Giant' },
-    { key: 'goliath_frost_giant', name: 'Frost Giant' },
-    { key: 'goliath_hill_giant', name: 'Hill Giant' },
-    { key: 'goliath_stone_giant', name: 'Stone Giant' },
-    { key: 'goliath_storm_giant', name: 'Storm Giant' },
+    { key: 'cloud_giant', name: 'Cloud Giant' },
+    { key: 'fire_giant', name: 'Fire Giant' },
+    { key: 'frost_giant', name: 'Frost Giant' },
+    { key: 'hill_giant', name: 'Hill Giant' },
+    { key: 'stone_giant', name: 'Stone Giant' },
+    { key: 'storm_giant', name: 'Storm Giant' },
   ]
 }, {
   key: 'halfling',
   name: 'Halfling',
-  updatedAt: new Date(),
 }, {
   key: 'human',
   name: 'Human',
-  updatedAt: new Date(),
 }, {
   key: 'kalashtar',
   name: 'Kalashtar',
-  updatedAt: new Date(),
 }, {
   key: 'khoravar',
   name: 'Khoravar',
-  updatedAt: new Date(),
 }, {
   key: 'orc',
   name: 'Orc',
-  updatedAt: new Date(),
 }, {
   key: 'shifter',
   name: 'Shifter',
-  updatedAt: new Date(),
   subspecies: [
-    { key: 'shifter_beasthide', name: 'Beasthide' },
-    { key: 'shifter_longtooth', name: 'Longtooth' },
-    { key: 'shifter_swiftstride', name: 'Swiftstride' },
-    { key: 'shifter_wildhunt', name: 'Wildhunt' },
+    { key: 'beasthide', name: 'Beasthide' },
+    { key: 'longtooth', name: 'Longtooth' },
+    { key: 'swiftstride', name: 'Swiftstride' },
+    { key: 'wildhunt', name: 'Wildhunt' },
   ]
 },{
   key: 'tiefling',
   name: 'Tiefling',
-  updatedAt: new Date(),
   subspecies: [
-    { key: 'tiefling_abyssal', name: 'Abyssal' },
-    { key: 'tiefling_chthonic', name: 'Chthonic' },
-    { key: 'tiefling_infernal', name: 'Infernal' },
+    { key: 'abyssal', name: 'Abyssal' },
+    { key: 'chthonic', name: 'Chthonic' },
+    { key: 'infernal', name: 'Infernal' },
   ]
 }, {
   key: 'warforged',
   name: 'Warforged',
-  updatedAt: new Date(),
 }];
 
-const dndClasses = [{
+const archtypes = [{
   key: 'artificer',
   name: 'Artificer',
-  primaryAbility: AbilityScore.INT,
-  updatedAt: new Date(),
-}, {
-  key: 'artificer',
-  name: 'Artificer',
-  primaryAbility: AbilityScore.INT,
-  updatedAt: new Date(),
+  spellcasting_ability: AbilityScore.INT,
 }, {
   key: 'barbarian',
   name: 'Barbarian',
-  primaryAbility: AbilityScore.STR,
-  updatedAt: new Date(),
 }, {
   key: 'bard',
   name: 'Bard',
-  primaryAbility: AbilityScore.CHA,
-  updatedAt: new Date(),
+  spellcasting_ability: AbilityScore.CHA,
 }, {
   key: 'blood_hunter',
   name: 'Blood Hunter',
-  primaryAbility: AbilityScore.WIS,
-  updatedAt: new Date(),
 }, {
   key: 'cleric',
   name: 'Cleric',
-  primaryAbility: AbilityScore.WIS,
-  updatedAt: new Date(),
+  spellcasting_ability: AbilityScore.WIS,
 }, {
   key: 'druid',
   name: 'Druid',
-  primaryAbility: AbilityScore.WIS,
-  updatedAt: new Date(),
+  spellcasting_ability: AbilityScore.WIS,
 }, {
   key: 'fighter',
   name: 'Fighter',
-  primaryAbility: AbilityScore.STR,
-  updatedAt: new Date(),
 }, {
   key: 'gunslinger',
   name: 'Gunslinger',
-  primaryAbility: AbilityScore.DEX,
-  updatedAt: new Date(),
 },  {
   key: 'illrigger',
   name: 'Illrigger',
-  primaryAbility: AbilityScore.CHA,
-  updatedAt: new Date(),
 }, {
   key: 'monk',
   name: 'Monk',
-  primaryAbility: AbilityScore.DEX,
-  updatedAt: new Date(),
 }, {
   key: 'monster_hunter',
   name: 'Monster Hunter',
-  primaryAbility: AbilityScore.STR,
-  updatedAt: new Date(),
 }, {
   key: 'paladin',
   name: 'Paladin',
-  primaryAbility: AbilityScore.STR,
-  updatedAt: new Date(),
+  spellcasting_ability: AbilityScore.CHA,
 }, {
   key: 'ranger',
   name: 'Ranger',
-  primaryAbility: AbilityScore.DEX,
-  updatedAt: new Date(),
+  spellcasting_ability: AbilityScore.WIS,
 }, {
   key: 'rogue',
   name: 'Rogue',
-  primaryAbility: AbilityScore.DEX,
-  updatedAt: new Date(),
 }, {
   key: 'sorcerer',
   name: 'Sorcerer',
-  primaryAbility: AbilityScore.CHA,
-  updatedAt: new Date(),
+  spellcasting_ability: AbilityScore.CHA,
 }, {
   key: 'warlock',
   name: 'Warlock',
-  primaryAbility: AbilityScore.CHA,
-  updatedAt: new Date(),
+  spellcasting_ability: AbilityScore.CHA,
 }, {
   key: 'wizard',
   name: 'Wizard',
-  primaryAbility: AbilityScore.INT,
-  updatedAt: new Date(),
+  spellcasting_ability: AbilityScore.INT,
 }];
 
 const playerCharacters = [{
 	id: 1,
 	name: 'Angry Spots',
-	dndClass: 'barbarian',
+	archtype: 'barbarian',
   species: 'shifter',
   subspecies: 'shifter_swiftstride',
 	url: process.env.SPOTS_URL || null,
-	updatedAt: new Date(),
 }, {
 	id: 2,
 	name: 'Venimous Micheal',
-	dndClass: 'bard',
+	archtype: 'bard',
   species: 'elf',
   subspecies: 'elf_high',
-	updatedAt: new Date(),
 }, {
 	id: 3,
 	name: 'Healer Henry',
-	dndClass: 'cleric',
+	archtype: 'cleric',
   species: 'human',
-	updatedAt: new Date(),
 }, {
 	id: 4,
 	name: 'Hexful Agatha',
-	dndClass: 'druid',
+	archtype: 'druid',
   species: 'hexblood',
   url: process.env.AGATHA_URL,
-	updatedAt: new Date(),
 }, {
 	id: 5,
 	name: 'Fighting Felix',
-	dndClass: 'fighter',
+	archtype: 'fighter',
   species: 'dwarf',
-	updatedAt: new Date(),
 }, {
 	id: 6,
 	name: 'Calm Max',
-	dndClass: 'monk',
+	archtype: 'monk',
   species: 'halfling',
-	updatedAt: new Date(),
 }, {
 	id: 7,
 	name: 'Rightous Dick',
-	dndClass: 'paladin',
+	archtype: 'paladin',
   species: 'goliath',
   subspecies: 'goliath_fire_giant',
-	updatedAt: new Date(),
 }, {
 	id: 8,
 	name: 'Watchful Bruce',
-	dndClass: 'ranger',
+	archtype: 'ranger',
   species: 'elf',
   subspecies: 'elf_wood',
-	updatedAt: new Date(),
 }, {
 	id: 9,
 	name: 'Tricky Ricky',
-	dndClass: 'rogue',
+	archtype: 'rogue',
   species: 'tiefling',
   subspecies: 'tiefling_infernal',
-	updatedAt: new Date(),
 }, {
 	id: 10,
 	name: 'Magic Mike',
-	dndClass: 'sorcerer',
+	archtype: 'sorcerer',
   species: 'aasimar',
-	updatedAt: new Date(),
 },  {
 	id: 11,
 	name: 'Fixit Futhark',
-	dndClass: 'warlock',
+	archtype: 'warlock',
   species: 'warforged',
   url: process.env.FUTHARK_URL,
-	updatedAt: new Date(),
 }, {
 	id: 12,
 	name: 'Analytical Aynrix',
-	dndClass: 'wizard',
+	archtype: 'wizard',
   species: 'human',
   url: process.env.AYNRIX_URL,
-	updatedAt: new Date(),
 }, {
 	id: 13,
 	name: 'Bloody Butcher',
-  dndClass: 'blood_hunter',
+  archtype: 'blood_hunter',
 	species: 'gnome',
   subspecies: 'gnome_forest',
-	updatedAt: new Date(),
 }, {
 	id: 14,
 	name: 'Deadey Ace',
-  dndClass: 'gunslinger',
+  archtype: 'gunslinger',
 	species: 'shifter',
   subspecies: 'shifter_wildhunt',
   url: process.env.ACER_URL,
-	updatedAt: new Date(),
 }, {
 	id: 15,
 	name: 'Lucifer Morningstar',
-  dndClass: 'illrigger',
+  archtype: 'illrigger',
 	species: 'tiefling',
   subspecies: 'tiefling_chthonic',
-	updatedAt: new Date(),
 }, {
 	id: 16,
 	name: 'Mad Maddy',
-  dndClass: 'monster_hunter',
+  archtype: 'monster_hunter',
 	species: 'dragonborn',
   subspecies: 'dragonborn_black',
-	updatedAt: new Date(),
 }]
 
 const prisma = new PrismaClient();
 
 async function main() {
-	const dndClassesTasks = dndClasses.map(async ({ name, key, primaryAbility }) => {
-
-
-    return prisma.class.upsert({
-      where: { key },
-      create: {
-        name,
-        key,
-        primaryAbility,
-      },
-      update: {
-        name,
-        key,
-        primaryAbility,
-      },
-    })
-});
+	const archtypeTasks = archtypes.map(async (
+    archtype: {
+      name: string;
+      key: string;
+      spellcasting_ability?: "STR" | "DEX" | "CON" | "INT" | "WIS" | "CHA" | undefined;
+    }
+  ) => prisma.archtype.upsert({
+      where: { key: archtype.key },
+      create: archtype,
+      update: archtype,
+    }));
   const speicesTasks = species.map(async ({ subspecies = [], ...speciesSingular }) => {
     const newSpecies = await prisma.species.upsert({
       where: { key: speciesSingular.key },
@@ -340,13 +272,13 @@ async function main() {
 
     const subspeciesTasks = subspecies.map(async (subspeciesSingular) => (
       prisma.subspecies.upsert({
-        where: { key: subspeciesSingular.key },
+        where: { key: `${subspeciesSingular.key}_${subspeciesSingular}` },
         create: {
-          speciesId: newSpecies.id,
+          species_id: newSpecies.id,
           ...subspeciesSingular,
         },
         update: {
-          speciesId: newSpecies.id,
+          species_id: newSpecies.id,
           ...subspeciesSingular,
         },
       })
@@ -360,30 +292,27 @@ async function main() {
     }
   });
   const speciesResult = await Promise.all(speicesTasks);
-  const dndClassesResult = await Promise.all(dndClassesTasks);
+  const archtypeesResult = await Promise.all(archtypeTasks);
 
 
 	const playerCharacterTasks = playerCharacters.map(
-    async ({ dndClass, species: speciesSingular,  subspecies: subspeciesSingular, ...playerCharacter }) => {
-      const dndClassItem = dndClassesResult.find(({ key }) => key === dndClass);
+    async ({ archtype, species: speciesSingular,  subspecies: subspeciesSingular, ...playerCharacter }) => {
+      const archtypeItem = archtypeesResult.find(({ key }) => key === archtype);
       const speciesItem = speciesResult.find(({ key }) => key === speciesSingular);
       const subspeciesItem = speciesItem?.subspecies.find(({ key }) => key === subspeciesSingular);
+
+      const data = {
+        archtype_id: archtypeItem?.id || 1,
+        species_id: speciesItem?.id || 1,
+        subspecies_id: subspeciesItem?.id || null,
+        ...playerCharacter
+      };
       
       
       return prisma.playerCharacter.upsert({
         where: { id: playerCharacter.id },
-        create: {
-          classId: dndClassItem?.id || 1,
-          speciesId: speciesItem?.id || 1,
-          subspeciesId: subspeciesItem?.id || null,
-          ...playerCharacter
-        },
-        update: {
-          classId: dndClassItem?.id || 1,
-          speciesId: speciesItem?.id || 1,
-          subspeciesId: subspeciesItem?.id || null,
-          ...playerCharacter
-        },
+        create: data,
+        update: data,
       })
     }
   );
